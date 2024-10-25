1ca90f8ef2594c8f935bec29c28a6fbeab1f33eb
sidikoff <olmassiddiqov55@gmail.com>
2024-10-25 20:27:34 +0500

    first commit

---
+++ 1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Siddiqov O'lmas";
+$firstname = "O'lmas";
+$lastname = "siddiqov";
+var_dump( $lastname);
+/**
+ * 
+ * 
+ */
+$a = 5;
+$b = 2;
+echo $a % $b;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo sqrt($b);
